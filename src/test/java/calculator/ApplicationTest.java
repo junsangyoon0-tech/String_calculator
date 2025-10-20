c2d71d5467fff6a4cc5b5cf7dcb920aaf35e9c1b
junsang <junsangyoon0@gmail.com>
2025-10-20 20:34:02 +0900

    feat: 문자열 덧셈 계산기 테스트 케이스 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -6,21 +6,38 @@ import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ApplicationTest extends NsTest {
+public class ApplicationTest extends NsTest {
+    @Test
+    public void add_null_또는_빈문자(){
+        assertEquals(0, Application.add(null));
+        assertEquals(0, Application.add(""));
+    }
+    @Test
+    public void add_숫자하나() throws Exception{
+        assertEquals(1, Application.add("1"));
+    }
+    @Test
+    public void add_쉼표구분자() throws Exception{
+        assertEquals(3, Application.add("1,2"));
+    }
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
         });
     }
-
+    @Test
+    public void add_custom_구분자() throws Exception{
+        assertEquals(6, Application.add("//;\n1;2;3"));
+    }
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> Application.add("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
